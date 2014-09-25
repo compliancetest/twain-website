e4ff8e04686bf0a03b1091acc5d9dab6170ec5fc
Ilya <ilhanhtml@gmail.com>
2014-09-25 18:02:47 +0300

    refs #4075 - Added View Pricing Plans button and fixed delimiter in feature plan table.

--- wp-content/themes/bp-child/single-test-suite.php
+++ wp-content/themes/bp-child/single-test-suite.php
@@ -295,27 +295,43 @@ Template Name Posts: Test Suite
                             <?php
                             } else if ($organisation = ct_get_user_organisation($user_id)) {
                             ?>
-                                <a class="pricing-plans-link action-btn blue-edit-btn" href="#pricing-plans" data-type="inline">View Pricing Plans</a>
-                                <a href="<?php echo the_permalink() ?>?_organisation_nonce=<?php echo wp_create_nonce('subscribe') ?>&suite_id=<?php echo $suite->id ?>" rel="custom-popup" cp-type="ajax" class="suite-subscript-link suite-subscript-link-oneline" cp-closeWhenClickOveraly=0 cp-removeBoxAfterClose=1>
-                                    <span class="text-b"><b>ACCESS</b><br />Test Harness</span>
-                                </a>
+                                <div class="test-suite-actions">
+                                    <a class="pricing-plans-link big-red-btn" href="#pricing-plans" data-type="inline">
+                                        <span class="price-b">
+                                            <span class="l"></span>
+                                            <span class="m">View Pricing<br>Plans</span>
+                                            <span class="r"></span>
+                                        </span>
+                                    </a>
+                                    <a href="<?php echo the_permalink() ?>?_organisation_nonce=<?php echo wp_create_nonce('subscribe') ?>&suite_id=<?php echo $suite->id ?>" rel="custom-popup" cp-type="ajax" class="suite-subscript-link suite-subscript-link-oneline" cp-closeWhenClickOveraly=0 cp-removeBoxAfterClose=1>
+                                        <span class="text-b"><b>ACCESS</b><br />Test Harness</span>
+                                    </a>
+                                </div>
                             <?php
                             } else {
                             ?>
-                                <a class="pricing-plans-link action-btn blue-edit-btn" href="#pricing-plans" data-type="inline">View Pricing Plans</a>
-                                <a class="pricing-plans-link suite-subscript-link" href="#pricing-plans" data-type="inline">
-<!--                                    <span class="price-b">-->
-<!--                                        <span class="l"></span>-->
-<!--                                        <span class="m"><b>$--><?php //echo $suite->monthlySubscriptionPriceValue?><!--</b><br />per month</span>-->
-<!--                                        <span class="r"></span>-->
-<!--                                    </span>-->
-<!--                                    <span class="price-b signup-price">-->
-<!--                                        <span class="l"></span>-->
-<!--                                        <span class="m"><b>$--><?php //echo $suite->signupPriceValue?><!--</b><br />sign-up fee</span>-->
-<!--                                        <span class="r"></span>-->
-<!--                                    </span>-->
-                                    <span class="text-b"><b>ACCESS</b><br />Test Harness</span>
-                                </a>
+                                <div class="test-suite-actions">
+                                    <a class="pricing-plans-link big-red-btn" href="#pricing-plans" data-type="inline">
+                                        <span class="price-b">
+                                            <span class="l"></span>
+                                            <span class="m">View Pricing<br>Plans</span>
+                                            <span class="r"></span>
+                                        </span>
+                                    </a>
+                                    <a class="pricing-plans-link suite-subscript-link" href="#pricing-plans" data-type="inline">
+    <!--                                    <span class="price-b">-->
+    <!--                                        <span class="l"></span>-->
+    <!--                                        <span class="m"><b>$--><?php //echo $suite->monthlySubscriptionPriceValue?><!--</b><br />per month</span>-->
+    <!--                                        <span class="r"></span>-->
+    <!--                                    </span>-->
+    <!--                                    <span class="price-b signup-price">-->
+    <!--                                        <span class="l"></span>-->
+    <!--                                        <span class="m"><b>$--><?php //echo $suite->signupPriceValue?><!--</b><br />sign-up fee</span>-->
+    <!--                                        <span class="r"></span>-->
+    <!--                                    </span>-->
+                                        <span class="text-b"><b>ACCESS</b><br />Test Harness</span>
+                                    </a>
+                                </div>
                             <?php
                             }
                     } else {
@@ -349,10 +365,18 @@ Template Name Posts: Test Suite
                 } else {
                     
             ?>
-                    <a class="pricing-plans-link action-btn blue-edit-btn" href="#pricing-plans" data-type="inline">View Pricing Plans</a>
+                <div class="test-suite-actions">
+                    <a class="pricing-plans-link big-red-btn" href="#pricing-plans" data-type="inline">
+                        <span class="price-b">
+                            <span class="l"></span>
+                            <span class="m">View Pricing<br> Plans</span>
+                            <span class="r"></span>
+                        </span>
+                    </a>
                     <a href="#registration-popup" rel="custom-popup" cp-type="inline" class="suite-subscript-link suite-subscript-link-oneline" cp-closeWhenClickOveraly=0>
                         <span class="text-b"><b>ACCESS</b><br />Test Harness</span>
                     </a>
+                </div>
             <?php
                 }                                
             ?>
