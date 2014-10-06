eb217f269f021376551dca81b46fde81ab9f20f2
eric <haohaneric@gmail.com>
2014-10-06 16:10:01 +0800

    Remove unused tables

--- sql_updates/51_drop_unused_tables.sql
+++ sql_updates/51_drop_unused_tables.sql
@@ -1,5 +1,11 @@
-DROP TABLE `wp_users_purchases`;
-DROP TABLE `wp_users_transactions`;
-DROP TABLE `wp_users_organisation_pricing`;
-DROP TABLE `wp_users_organisation_subscriptions`;
-DROP TABLE `wp_users_payments_logs`;
+#DROP TABLE `wp_users_purchases`;
+#DROP TABLE `wp_users_transactions`;
+#DROP TABLE `wp_users_organisation_pricing`;
+#DROP TABLE `wp_users_organisation_subscriptions`;
+#DROP TABLE `wp_users_payments_logs`;
+
+RENAME TABLE `wp_users_purchases` TO `wp_users_purchases_deleted`;
+RENAME TABLE `wp_users_transactions` TO `wp_users_transactions_deleted`;
+RENAME TABLE `wp_users_organisation_pricing` TO `wp_users_organisation_pricing_deleted`;
+RENAME TABLE `wp_users_organisation_subscriptions` TO `wp_users_organisation_subscriptions_deleted`;
+RENAME TABLE `wp_users_payments_logs` TO `wp_users_payments_logs_deleted`;
