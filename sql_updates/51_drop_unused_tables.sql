126605a8a009bb6da1766dcb542a299319d971ab
eric <haohaneric@gmail.com>
2014-10-06 15:17:42 +0800

    Remove unused/superceded tables

--- sql_updates/51_drop_unused_tables.sql
+++ sql_updates/51_drop_unused_tables.sql
@@ -2,4 +2,4 @@ DROP TABLE `wp_users_purchases`;
 DROP TABLE `wp_users_transactions`;
 DROP TABLE `wp_users_organisation_pricing`;
 DROP TABLE `wp_users_organisation_subscriptions`;
-DROP TABLE `wp_users_payment_logs`;
+DROP TABLE `wp_users_payments_logs`;
