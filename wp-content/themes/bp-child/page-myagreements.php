662a2f4e54e3e3eca0125a448825324c933b2f5a
Ivan Solowjew <ivansolowjew@gmail.com>
2014-11-11 15:08:00 +0200

    #4426 Create / Edit Service UI Layout Improvements

--- wp-content/themes/bp-child/page-myagreements.php
+++ wp-content/themes/bp-child/page-myagreements.php
@@ -112,7 +112,7 @@ get_header();
                                                 $suite = new TestSuite( $serv->service_suite_id );
                                                 $suite->load();
                                            ?>
-                                           <li>Process: <strong><?php echo Process::get_full_name( Process::get_process_by_id( $suite->process ) );?></strong></li>
+                                           <li>Suite: <strong><a href="<?php echo get_permalink( $service->service_suite_id  );?>"><?php echo get_the_title( $serv->service_suite_id  ) ;?></strong></a></li>
                                            <li>Role: <strong><?php echo implode( ', ', $serv->service_roles );?></strong></li>
     <!--                                       <li>ServiceID: <strong>--><?php //echo $serv->service_type.':'.$serv->service_id;?><!--</strong></li>-->
                                            <li><?php echo $serv->service_type ?>: <strong><?php echo $serv->service_id ?></strong></li>
