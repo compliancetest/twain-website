206ccfb8859c41aecfd075d8931c1720d022c8a3
Ilya <ilhanhtml@gmail.com>
2014-11-18 22:50:34 +0200

    #4453: Fixes for agreement table view.

--- wp-content/themes/bp-child/page-myagreements.php
+++ wp-content/themes/bp-child/page-myagreements.php
@@ -84,17 +84,17 @@ get_header();
                                    </div>
                                    <div class="td td-requester-data">
                                        <a href="<?php echo get_permalink( $req_serv->id );?>"><?php echo get_the_title( $req_serv->id );?></a>
-                                       </br>
+                                       <br />
                                        <?php echo $req_serv->service_owner;?>
                                    </div>
                                    <div class="td td-responder-data">
                                        <a href="<?php echo get_permalink( $res_serv->id );?>"><?php echo get_the_title( $res_serv->id );?></a>
-                                       </br>
+                                       <br />
                                        <?php echo $res_serv->service_owner;?>
                                    </div>
                                    <div class="td td-status"><span class="status-<?php echo strtolower( $agreement->status );?>"><?php echo $agreement->status;?></span></div>
                                    <div class="td td-actions centered">
-                                       <a href="<?php echo get_site_url()?>?_psnonce=<?php echo wp_create_nonce('delete-agreement')?>&id=<?php echo $agreement->id?>" class="action-btn delete-btn icon-btn delete_service has-tooltip centered" href="#" style="margin-left: 30px; margin-top: 10px;"><span class="p"></span><span class="simple_tooltip radius6" style="top: -40px;">Delete Agreement<span></span></span></a>
+                                       <a href="<?php echo get_site_url()?>?_psnonce=<?php echo wp_create_nonce('delete-agreement')?>&id=<?php echo $agreement->id?>" class="action-btn delete-btn icon-btn delete_service has-tooltip" href="#" title="Delete Agreement"><span class="p"></span></a>
                                    </div>
 
                                </div>
