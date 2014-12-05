c66a7ce539ef9fecd0dc07db953e94ce1aca06a4
Ivan Solowjew <ivansolowjew@gmail.com>
2014-12-05 16:15:12 +0200

    #4489 Move Front End blob storage to S3

--- wp-content/themes/bp-child/content/ticket-detail.php
+++ wp-content/themes/bp-child/content/ticket-detail.php
@@ -55,7 +55,7 @@
         <div class="ticket-attachments">
             <?php $attachments = getAttachmentsByTicketId( $ticket_id ); ?>
             <?php foreach($attachments as $file): ?>
-                <a href="<?php echo S3Wrapper::getAttachmentLink( $file->token,  pathinfo( $file->file_name, PATHINFO_EXTENSION ), 'ticket', true );?>"><?php echo $file->file_name?></a><br />
+                <a href="<?php echo S3Wrapper::getAttachmentLink( $file->token,  pathinfo( $file->file_name, PATHINFO_EXTENSION ), 'tickets', true );?>"><?php echo $file->file_name?></a><br />
             <?php endforeach; ?>
         </div>
     <?php endif; ?>
