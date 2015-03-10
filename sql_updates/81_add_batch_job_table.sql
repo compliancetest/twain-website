35bc921f92cb572104cb1a9f53c679109ac9540b
Ivan Solowjew <ivansolowjew@gmail.com>
2015-03-10 12:01:54 +0200

    #4915 Batch jobs for generating Testing progress report and mailing it to selected recipients

--- sql_updates/81_add_batch_job_table.sql
+++ sql_updates/81_add_batch_job_table.sql
@@ -14,3 +14,28 @@ CREATE TABLE IF NOT EXISTS `wp_batch_jobs_params` (
   `value` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+INSERT INTO `wp_batch_jobs` (`id`, `identifier`, `access_key`, `function_name`, `is_active`)
+VALUES (NULL, 'GENERATE_TESTING_REPORTS', '69587649cb4f2706c0d5a35f8d49a04b', 'generateTestingReport', '1');
+
+INSERT INTO `wp_batch_jobs` (
+`id` ,
+`identifier` ,
+`access_key` ,
+`function_name` ,
+`is_active`
+)
+VALUES (
+NULL , 'SEND_TESTING_REPORTS', '18a12b6e422eb0ee99d837b94dd47fbd', 'notifyUsers', '1'
+);
+INSERT INTO `wp_batch_jobs_params` (
+`id` ,
+`batch_job_id` ,
+`name` ,
+`value`
+)
+VALUES (
+NULL , '3', 'emails', 'ivansolowjew@gmail.com'
+), (
+NULL , '3', 'community', 'SuperStream'
+);
