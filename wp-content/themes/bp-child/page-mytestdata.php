87bc66624868567942db78d887f5d54e029e58b0
Ivan Solowjew <ivansolowjew@gmail.com>
2015-07-13 01:10:09 +0300

    #5376 Create UI for Schedule Capability - fixes && improvements

--- wp-content/themes/bp-child/page-mytestdata.php
+++ wp-content/themes/bp-child/page-mytestdata.php
@@ -14,6 +14,7 @@ $subscriptions =  getUserSubscriptions(null, true);
 $sqs_validation_enabled = get_option('validate_via_sqs') == 'yes' ? true : false;
 $filters = getCustomerProfileInstancesFilters($profileInstances);
 $doesPricingPlanSupportBulk = PricingPlan::isSupportBulk();
+$doesGatewayConfigured = \User\User::isGatewayConfigured();
 ?>
 <div class="content" id="my_testdata">
     <div class="dashboard-tabs">
@@ -150,9 +151,9 @@ $doesPricingPlanSupportBulk = PricingPlan::isSupportBulk();
                                         <a href="javascript:void(0);" class="action-btn icon-btn upload-btn has-tooltip greyed-out-btn"><span class="p"></span><span class="simple_tooltip radius6">Upload Profile<span></span></span></a>
                                     <?php endif;?>
                                     <a href="<?php echo S3Wrapper::getProfileLink( $instance->token, $instance->profile_name, true );?>" class="left10 action-btn icon-btn download-btn has-tooltip"><span class="p"></span><span class="simple_tooltip radius6">Download Profile<span></span></span></a>
-                                    <?php if( ProfileInstance::isSchedule( $instance->profile_role ) && $instance->validation_status == 'valid' ):?>
+                                    <?php if( ProfileInstance::isSchedule( $instance->profile_role ) && $instance->validation_status == 'valid' && $doesGatewayConfigured && $doesPricingPlanSupportBulk):?>
                                             <a href="/my-profile?td-action=<?php echo wp_create_nonce('prepare_schedule')?>&id=<?php echo $instance->id?>" rel="custom-popup" cp-type="ajax" cp-removeBoxAfterClose=1 class="action-btn icon-btn blue-btn expand-btn left10 has-tooltip prepare_schedule trigger-btn"><span class="p"></span><span class="simple_tooltip radius6" style="width: 150px; left: -24px;">Create a Run profile from this Schedule<span></span></span></a>
-                                    <?php elseif( ProfileInstance::isRun( $instance->profile_role ) && $doesPricingPlanSupportBulk && ProfileInstance::isRunCanBeTriggered( $instance->token ) && \User\User::isGatewayConfigured()):?>
+                                    <?php elseif( ProfileInstance::isRun( $instance->profile_role ) && $doesPricingPlanSupportBulk && ProfileInstance::isRunCanBeTriggered( $instance->token ) && $doesGatewayConfigured ):?>
                                         <a href="<?php echo the_permalink() ?>?td-action=<?php echo wp_create_nonce('trigger_run')?>&id=<?php echo $instance->id?>" rel="custom-popup" cp-type="ajax" cp-removeBoxAfterClose=1 class="action-btn icon-btn blue-btn expand-btn left10 has-tooltip trigger-btn"><span class="p"></span><span class="simple_tooltip radius6" style="width: 150px; left: -24px;">Trigger Run<span></span></span></a>
                                     <?php elseif( $sqs_validation_enabled && $instance->is_expanded == 0 && $instance->validation_status == 'valid' && in_array( str_replace( ' ', '', $instance->profile_role ), ProfileType::getExpandableTypes() ) ): ?>
                                         <a href="#create-expanded-version" data-id="<?php echo $instance->id;?>" class="action-btn icon-btn blue-btn expand-btn left10 has-tooltip create_expanded_version"><span class="p"></span><span class="simple_tooltip radius6">Create Expanded Version of this profile<span></span></span></a>
