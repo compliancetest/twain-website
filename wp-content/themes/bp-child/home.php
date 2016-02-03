b3cf7d772735119ca3e66d2c755cde6f4ddd97e3
Ivan Solowjew <ivansolowjew@gmail.com>
2016-02-03 11:12:16 +0200

    #5817 Further homepage changes

--- wp-content/themes/bp-child/home.php
+++ wp-content/themes/bp-child/home.php
@@ -38,7 +38,6 @@
         <ul class="tabs-header">
             <li><a href="#software-vendors-panel"><span class="tab-icon software-vendors-icon"></span><?php echo $home_settings['tab_1_title']; ?> <em><?php echo $home_settings['tab_1_subtitle']; ?></em></a></li>
             <li><a href="#organisations-panel"><span class="tab-icon organisations-icon"></span><?php echo $home_settings['tab_2_title']; ?> <em><?php echo $home_settings['tab_2_subtitle']; ?></em></a></li>
-            <li><a href="#standard-issuers-panel"><span class="tab-icon standard-issuers-icon"></span><?php echo $home_settings['tab_3_title']; ?> <em><?php echo $home_settings['tab_3_subtitle']; ?></em></a></li>
         </ul>
         <div class="tabs-content">
             <div class="wrapper">
@@ -60,16 +59,6 @@
                     </div>
                     <div class="tab-content-image">
                         <img src="<?php echo $home_settings['tab_2_image']; ?>" alt=""/>
-                    </div>
-                </div>
-                <div id="standard-issuers-panel">
-                    <div class="tab-content-text">
-                        <?php echo $home_settings['tab_3_content']; ?>
-                        <br>
-                        <a href="<?php echo $home_settings['tab_3_button_link']; ?>" class="view-sample-btn">View Sample</a>
-                    </div>
-                    <div class="tab-content-image">
-                        <img src="<?php echo $home_settings['tab_3_image']; ?>" alt=""/>
                     </div>
                 </div>
             </div>
