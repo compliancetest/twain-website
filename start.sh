80959c0ae45efc33d0cac6557d8f59495708c735
alm@ct01 <alm@ct01>
2016-03-21 14:04:42 +0000

    changes in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#cd /var/www/html/laravel
-#/usr/local/bin/composer install --prefer-source --no-interaction
-#/usr/local/bin/composer update
+cd /var/www/html/laravel
+/usr/local/bin/composer install --prefer-source --no-interaction
+/usr/local/bin/composer update
 
 /usr/sbin/apache2ctl -D FOREGROUND
