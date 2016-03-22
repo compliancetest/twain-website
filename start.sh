bb74cb0828168997432fd1da66f7be61449d458f
Ivan Solowjew <ivansolowjew@gmail.com>
2016-03-22 17:16:06 +0200

    added laravel installation

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#cd /var/www/html/laravel
+cd /var/www/html/laravel
 #/usr/local/bin/composer install --prefer-source --no-interaction
-#/usr/local/bin/composer update
+/usr/local/bin/composer update
 
 /usr/sbin/apache2ctl -D FOREGROUND
