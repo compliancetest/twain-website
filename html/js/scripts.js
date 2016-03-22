109ba59eeb479a9e6603ad5265d7fef166139d49
Ilya <ilhanhtml@gmail.com>
2016-03-22 08:28:23 +0200

    refs #5798 - Responsive templates for community pages

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function($) {
 
-    $('[data-tooltip="tooltip"]').tooltip()
+    $('[data-tooltip="tooltip"]').tooltip();
+
+
+    $('#confirmRemoveMembership').on('show.bs.modal', function(e) {
+        $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+    });
+
 });
 
 
