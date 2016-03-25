13df81abf51d55a811d829fad2c5b2989e2d02e8
Ivan Solowjew <ivansolowjew@gmail.com>
2016-03-25 22:47:40 +0200

    deployment issue

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ cd /var/www/html/laravel
 #/usr/local/bin/composer install --prefer-source --no-interaction
 /usr/local/bin/composer update
 
-/usr/sbin/apache2ctl -D FOREGROUND
+#/usr/sbin/apache2ctl -D FOREGROUND
+sudo service apache2 restart
 tail -f /var/log/apache2/error.log
