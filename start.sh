324cdfbe056f87a78161e43844dcd715858b44f0
alm@ct01 <alm@ct01>
2016-03-29 17:26:56 +0000

    adding chmods to start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/usr/sbin/apache2ctl -D FOREGROUND
-
 chmod -R 777 /var/www/html/laravel/storage/logs
 chmod -R 777 /var/www/html/laravel/bootstrap/cache
 chmod -R 777 /var/www/html/laravel/storage/framework/views
 chmod -R 777 /var/www/html/laravel/storage/framework/sessions
+
+/usr/sbin/apache2ctl -D FOREGROUND
