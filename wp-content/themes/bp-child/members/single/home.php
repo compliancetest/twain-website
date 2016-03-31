143a107f026e588c431161b1c949646d064a4d7c
Ivan Solowjew <ivansolowjew@gmail.com>
2016-03-31 22:55:47 +0300

    #5965 Profile: empty section is displayed in public profile

--- wp-content/themes/bp-child/members/single/home.php
+++ wp-content/themes/bp-child/members/single/home.php
@@ -27,54 +27,54 @@ get_header( 'buddypress' ); ?>
 
 			</div><!-- #item-header -->
             <div class="column">
-                <div class="tabs_wrap light_gray_bcg radius6">
-                    <?php
-                        $currentTab = '';
-                        if(bp_is_user_profile()){
-                            $currentTab = 'profile';
-                        }else if(bp_is_user_messages()){
-                            $currentTab = 'message';
-                        }
-                    ?>
-			        <div id="item-body">                    
-				        <?php do_action( 'bp_before_member_body' );
-
-				        if ( bp_is_user_activity() || !bp_current_component() ) :
-					        locate_template( array( 'members/single/activity.php'  ), true );
-
-				         elseif ( bp_is_user_blogs() ) :
-					        locate_template( array( 'members/single/blogs.php'     ), true );
-
-				        elseif ( bp_is_user_friends() ) :
-					        locate_template( array( 'members/single/friends.php'   ), true );
-
-				        elseif ( bp_is_user_groups() ) :
-					        locate_template( array( 'members/single/groups.php'    ), true );
-
-				        elseif ( bp_is_user_messages() ) :
-					        locate_template( array( 'members/single/messages.php'  ), true );
-
-				        elseif ( bp_is_user_profile() ) :
-					        locate_template( array( 'members/single/profile.php'   ), true );
-
-				        elseif ( bp_is_user_forums() ) :
-					        locate_template( array( 'members/single/forums.php'    ), true );
-
-				        elseif ( bp_is_user_settings() ) :
-					        locate_template( array( 'members/single/settings.php'  ), true );
-
-				        // If nothing sticks, load a generic template
-				        else :
-					        locate_template( array( 'members/single/plugins.php'   ), true );
-
-				        endif;
-
-				        do_action( 'bp_after_member_body' ); ?>
-
-			        </div><!-- #item-body -->
-
-			        <?php do_action( 'bp_after_member_home_content' ); ?>
-                </div>
+<!--                <div class="tabs_wrap light_gray_bcg radius6">-->
+<!--                    --><?php
+//                        $currentTab = '';
+//                        if(bp_is_user_profile()){
+//                            $currentTab = 'profile';
+//                        }else if(bp_is_user_messages()){
+//                            $currentTab = 'message';
+//                        }
+//                    ?>
+<!--			        <div id="item-body">                    -->
+<!--				        --><?php //do_action( 'bp_before_member_body' );
+//
+//				        if ( bp_is_user_activity() || !bp_current_component() ) :
+//					        locate_template( array( 'members/single/activity.php'  ), true );
+//
+//				         elseif ( bp_is_user_blogs() ) :
+//					        locate_template( array( 'members/single/blogs.php'     ), true );
+//
+//				        elseif ( bp_is_user_friends() ) :
+//					        locate_template( array( 'members/single/friends.php'   ), true );
+//
+//				        elseif ( bp_is_user_groups() ) :
+//					        locate_template( array( 'members/single/groups.php'    ), true );
+//
+//				        elseif ( bp_is_user_messages() ) :
+//					        locate_template( array( 'members/single/messages.php'  ), true );
+//
+//				        elseif ( bp_is_user_profile() ) :
+//					        locate_template( array( 'members/single/profile.php'   ), true );
+//
+//				        elseif ( bp_is_user_forums() ) :
+//					        locate_template( array( 'members/single/forums.php'    ), true );
+//
+//				        elseif ( bp_is_user_settings() ) :
+//					        locate_template( array( 'members/single/settings.php'  ), true );
+//
+//				        // If nothing sticks, load a generic template
+//				        else :
+//					        locate_template( array( 'members/single/plugins.php'   ), true );
+//
+//				        endif;
+//
+//				        do_action( 'bp_after_member_body' ); ?>
+<!---->
+<!--			        </div><!-- #item-body -->
+<!---->
+<!--			        --><?php //do_action( 'bp_after_member_home_content' ); ?>
+<!--                </div>-->
             </div>
             <div class="clear"></div>
 		</div><!-- .padder -->
