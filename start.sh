99849fbff5a38bf5b97d0e13bf96c834fdd41da9
Ivan Solowjew <ivansolowjew@gmail.com>
2016-04-05 13:09:22 +0300

    #5922 Create REST Service for POST test results - added code to process sqs messages

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ chmod -R 777 /var/www/html/laravel/storage/logs
 chmod -R 777 /var/www/html/laravel/bootstrap/cache
 chmod -R 777 /var/www/html/laravel/storage/framework/views
 chmod -R 777 /var/www/html/laravel/storage/framework/sessions
+chmod -R 777 /var/www/html/laravel/storage/app/public/transactions/
 
 /usr/sbin/apache2ctl -D FOREGROUND
