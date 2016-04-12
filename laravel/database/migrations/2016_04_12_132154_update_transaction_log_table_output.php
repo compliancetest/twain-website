4b24997a6b7059400439b8eafa8c4edc095860fb
Ivan Solowjew <ivansolowjew@gmail.com>
2016-04-12 23:43:48 +0300

    trigger build

--- laravel/database/migrations/2016_04_12_132154_update_transaction_log_table_output.php
+++ laravel/database/migrations/2016_04_12_132154_update_transaction_log_table_output.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTransactionLogTable extends Migration
+class UpdateTransactionLogTableOutput extends Migration
 {
     /**
      * Run the migrations.
