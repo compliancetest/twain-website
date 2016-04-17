c3bc4aa615798ee45ab5271fadbde0154f7edfe7
Ilya <ilhanhtml@gmail.com>
2016-04-17 19:53:55 +0300

    refs #5798 - Test data view and edit

--- laravel/resources/views/pages/communities/partials/show/testdata.blade.php
+++ laravel/resources/views/pages/communities/partials/show/testdata.blade.php
@@ -38,8 +38,41 @@ $instances = getCommunityProfileInstatnces($community->id);
                 @foreach( $instances AS $instance )
                 <tr>
                     <td>
-                        <a href="{!! get_site_url() !!}/html/temp/test-data-profile-ajax.php" data-toggle="modal" data-remote="true" data-ajax-modal data-target="#modalViewProfile-{{ $instance->id }}">{{ $instance->profile_name }}</a>
+                        <a href="#modalCopyProfileUrl-{{ $instance->id }}" data-toggle="modal">{{ $instance->profile_name }}</a>
                         <p>{{ $instance->profile_description }}</p>
+
+                        {{-- Copy Profile URL Modal--}}
+                        <div class="modal fade" id="modalCopyProfileUrl-{{ $instance->id }}" tabindex="-1" role="dialog">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close-modal" data-tooltip="tooltip" title="Close popup" data-placement="left" data-dismiss="modal" aria-label="Close">Close</button>
+                                        Profile Instance Detail
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="form-group row">
+                                            <div class="col-sm-9">
+                                                {{--@@todo-ivan: Add profile id--}}
+                                                <input type="text" class="form-control" readonly id="profile-link-{{ $instance->id }}" value="http://twain.lc/get-profile?id={TODOADDPROFILEID}" />
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <button class="btn btn-success btn-with-icon btn-confirm copyProfileLink" data-clipboard-target="#profile-link-{{ $instance->id }}">Copy URL</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <a href="#" class="btn btn-success btn-with-icon btn-confirm">Download</a>
+                                        <a href="#" class="btn btn-default btn-with-icon btn-cancel" data-dismiss="modal">Cancel</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>{{ $instance->purpose }}</td>
+                    <td class="text-center">
+                        {{--<a href="{!! get_site_url() !!}?td-action={{ wp_create_nonce('view-profile-type') }}&id={{ $instance->type_id }}">{{ $instance->type_name }}</a>--}}
+                        <a href="{!! get_site_url() !!}/html/temp/test-data-profile-ajax.php" data-toggle="modal" data-remote="true" data-ajax-modal data-target="#modalViewProfile-{{ $instance->id }}">{{ $instance->type_name }}</a>
+
                         {{-- View Profile Modal--}}
                         <div class="modal fade profile-modal" id="modalViewProfile-{{ $instance->id }}" tabindex="-1" role="dialog">
                             <div class="modal-dialog" role="document">
@@ -58,9 +91,8 @@ $instances = getCommunityProfileInstatnces($community->id);
                                 </div>
                             </div>
                         </div>
+
                     </td>
-                    <td>{{ $instance->purpose }}</td>
-                    <td class="text-center"><a href="{!! get_site_url() !!}?td-action={{ wp_create_nonce('view-profile-type') }}&id={{ $instance->type_id }}">{{ $instance->type_name }}</a></td>
                     <td class="text-center">{{ formatDate($instance->created_date) }}</td>
                     <td class="text-center"><span class="item-{{ strtolower($instance->validation_status) }}"></span></td>
                     <td class="text-center text-nowrap">
@@ -68,7 +100,7 @@ $instances = getCommunityProfileInstatnces($community->id);
                             <a href="/html/temp/test-data-edit-profile-ajax.php" class="btn btn-icon btn-primary btn-edit" data-toggle="modal" data-remote="true" data-ajax-modal data-target="#modalEditProfile-{{ $instance->id }}" data-tooltip="tooltip" title="Edit Profile"></a>
                             <a href="#" class="btn btn-icon btn-danger btn-delete" data-tooltip="tooltip" title="Delete Profile"></a>
 
-                            {{-- View Profile Modal--}}
+                            {{-- Edit Profile Modal--}}
                             <div class="modal fade profile-modal" id="modalEditProfile-{{ $instance->id }}" tabindex="-1" role="dialog">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
@@ -80,7 +112,7 @@ $instances = getCommunityProfileInstatnces($community->id);
                                             <div class="block-loading"><div class="loading-content"><span class="loader"></span><div class="loading-text">LOADING DATA</div><div class="loading-wait">Please wait...</div></div></div>
                                         </div>
                                         <div class="modal-footer">
-                                            <a href="#" class="btn btn-success btn-with-icon btn-confirm">Download</a>
+                                            <a href="#" class="btn btn-success btn-with-icon btn-confirm">Confirm</a>
                                             <a href="#" class="btn btn-default btn-with-icon btn-cancel" data-dismiss="modal">Cancel</a>
                                         </div>
                                     </div>
@@ -97,3 +129,12 @@ $instances = getCommunityProfileInstatnces($community->id);
         </div>
     </div>
 </div>
+<script>
+    var clipboard = new Clipboard('.copyProfileLink');
+    clipboard.on('success', function(e) {
+        jQuery(e.trigger).parents('.form-group').before( '<div class="success-message">The profile url has been copied to clipboard.</div>');
+        var messageTimeoutHandler = setTimeout(function() {
+            $('.success-message').slideUp();
+        }, 2000);
+    });
+</script>
