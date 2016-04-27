6d1afd19c83bb012767e76d59494ac73e59cfb8f
Ivan Solowjew <ivansolowjew@gmail.com>
2016-04-27 19:12:02 +0300

    Edit profile screen fix

--- laravel/app/Http/Controllers/ProfilesController.php
+++ laravel/app/Http/Controllers/ProfilesController.php
@@ -82,7 +82,6 @@ class ProfilesController extends Controller
 
     public function update($communitySlug, $profileId, Request $request)
     {
-        dd($request->all());
         $community = Community::findBySlug($communitySlug);
         $profile = Profile::find($profileId);
         if ($community->isAdmin() || $profile->creator_id = Auth::user()->ID) {
