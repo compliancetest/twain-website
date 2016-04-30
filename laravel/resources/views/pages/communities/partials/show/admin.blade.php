75cfea2092de474459d5bad4dc409158093b86a9
Ivan Solowjew <ivansolowjew@gmail.com>
2016-04-30 21:35:30 +0300

    #6231 Invite to community feature - minor fix

--- laravel/resources/views/pages/communities/partials/show/admin.blade.php
+++ laravel/resources/views/pages/communities/partials/show/admin.blade.php
@@ -300,7 +300,7 @@
                                     <th class="text-center">Invitation Date</th>
                                     <th class="text-center">Registration Date</th>
                                 </tr>
-                                @if($invitedUsers)
+                                @if(count($invitedUsers) > 0)
                                     @foreach($invitedUsers as $invitedUser)
                                         <tr>
                                             <td>
@@ -319,7 +319,9 @@
                                         </tr>
                                     @endforeach
                                 @else
-
+                                    <tr>
+                                        <td colspan="4" class="text-center">No data yet</td>
+                                    </tr>
                                 @endif
                             </table>
                         </div>
