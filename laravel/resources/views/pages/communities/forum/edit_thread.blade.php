a7cd47aa11768e4ae09d767fc4009dbae5485933
Ilya <ilhanhtml@gmail.com>
2016-05-06 19:46:45 +0300

    #6247 - Fixed buttons for edit thread/post

--- laravel/resources/views/pages/communities/forum/edit_thread.blade.php
+++ laravel/resources/views/pages/communities/forum/edit_thread.blade.php
@@ -23,7 +23,7 @@
     <div class="form-actions clearfix">
 
         <div class="pull-right">
-            <button type="submit" class="btn btn-success btn-with-icon btn-upload" id="save_new_thread">Save</button>
+            <button type="submit" class="btn btn-success btn-with-icon btn-upload" id="save_new_thread">Confirm</button>
             <button type="button" class="btn btn-default btn-with-icon btn-cancel" id="cancel-edit-thread">Cancel</button>
         </div>
     </div>
