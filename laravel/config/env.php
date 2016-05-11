45dd49c742793eb62769819cda2713004b26e477
Ivan Solowjew <ivansolowjew@gmail.com>
2016-05-11 11:53:55 +0300

    #6123 Accept bucket configuration via environment variables

--- laravel/config/env.php
+++ laravel/config/env.php
@@ -4,7 +4,7 @@ $data = [
     'bucket' => [
         'transactions' => getenv('BUCKET_TRANSACTIONS') ? getenv('BUCKET_TRANSACTIONS') : 'data.twain.gosource.com.au',
         'website' => getenv('BUCKET_WEBSITE') ? getenv('BUCKET_WEBSITE') : 'data.twain.gosource.com.au',
-        'region' => getenv('BUCKET_REGION') ? getenv('BUCKET_WEBSITE') : 'us-west-2',
+        'region' => getenv('BUCKET_REGION') ? getenv('BUCKET_REGION') : 'us-west-2',
     ],
     'env' => getenv('ENVIRONMENT')
 ];
