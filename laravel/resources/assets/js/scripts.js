b7efc1ec63e2ada7d6eafa1c671dfcfc6d162baf
Ilya <ilhanhtml@gmail.com>
2016-06-18 15:00:38 +0300

    #6515 - Temp commit for debug issue on integration.

--- laravel/resources/assets/js/scripts.js
+++ laravel/resources/assets/js/scripts.js
@@ -549,6 +549,8 @@ var Page = {
                 },
                 success: function(rsp, status, jqXHR){
                     if(form.attr('id') == 'invite-user-form'){
+                        console.log('Invitation response');
+                        console.log(rsp.data);
                         $('.invitations_table').append('<tr><td>' + rsp.data.invitation_email+ '</td>' +
                                             '<td>' +rsp.data.first_name+ ' ' +rsp.data.last_name+'</td>' +
                                             '<td>' + rsp.data.created_at +'</td>' +
