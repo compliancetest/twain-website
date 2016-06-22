24306e11688d31858ee078236cae1cfbc8ac7e2a
Ivan Solowjew <ivansolowjew@gmail.com>
2016-06-22 17:06:35 +0300

    #18 Date filter is not working on the Transaction Log Page - fix filter range

--- wp-content/themes/bp-child/functions/classes/TransactionLogs.php
+++ wp-content/themes/bp-child/functions/classes/TransactionLogs.php
@@ -26,7 +26,7 @@ class TransactionLogs
             $this->where[] = sprintf(' subscription_id = %d ', $filters['subscription_id']);
         }
         if ($filters['date']) {
-            $this->where[] = " t.updated_at LIKE '" . date('Y-m-d', getUTCTimeStamp($filters['date'])) . "%' ";
+            $this->where[] = " ( t.updated_at > '" . date('Y-m-d H:i:s', getUTCTimeStamp($filters['date'])) . "' AND t.updated_at <  '" . date('Y-m-d H:i:s', getUTCTimeStamp($filters['date'] . ' 23:59:59')) . "' ) ";
         }
         if ($filters['outcome']) {
             $this->where[] = sprintf(" test_outcome_status_id = '%s' ", $filters['outcome']);
