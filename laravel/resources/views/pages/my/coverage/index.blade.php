b36ff82156bc2393bd91f50749e3a82dd81a9c70
Ivan Solowjew <ivansolowjew@gmail.com>
2016-06-23 14:00:25 +0300

    #36 Add a version to product on Coverage and Transaction tab

--- laravel/resources/views/pages/my/coverage/index.blade.php
+++ laravel/resources/views/pages/my/coverage/index.blade.php
@@ -46,7 +46,10 @@
                                     @if($userSuite['testPlans'])
                                         @foreach($userSuite['testPlans'] as $userPlan)
                                             <tr id="coverage-plan-{{ $userPlan['testPlan']->id }}">
-                                                <td class="text-nowrap">{{ $userPlan['product']->post_title . ' v' . $userPlan['product']->getMetaByKey('product_version') }}</td>
+                                                <td class="text-nowrap">
+                                                    {{ $userPlan['product']->post_title }}
+                                                    @if($userPlan['product']) {{ ' v' . $userPlan['product']->getMetaByKey('product_version') }}@endif
+                                                </td>
                                                 <td class="text-center">{{ $userPlan['testPlan']->level }}</td>
                                                 <td>{{ $userPlan['testPlan']->role }}</td>
                                                 <td>
