df8d9099951519f2a9c6849a8657f686c0160cdf
Ivan Solowjew <ivansolowjew@gmail.com>
2016-06-28 14:59:40 +0300

    #36 Add a version to product on Coverage and Transaction tab

--- laravel/resources/views/pages/my/coverage/edit.blade.php
+++ laravel/resources/views/pages/my/coverage/edit.blade.php
@@ -10,7 +10,7 @@
             <select name="product_id" id="availableProducts" class="form-control">
                 <option value="">Select a Product</option>
                 @foreach($products as $product)
-                    <option value="{{ $product->ID }}" @if($product->ID == $testPlan->product_id) selected="selected" @endif>{{ $product->post_title }}</option>
+                    <option value="{{ $product->ID }}" @if($product->ID == $testPlan->product_id) selected="selected" @endif>{{ $product->post_title . ' V' .$product->version }}</option>
                 @endforeach
             </select>
         </div>
