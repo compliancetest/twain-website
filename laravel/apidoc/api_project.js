c8ab56418c0b7f8a4863d7a2452fcb6edbbea015
Ivan Solowjew <ivansolowjew@gmail.com>
2016-07-12 10:54:34 +0300

    #47 Configure product feature list via REST API - added additional validation

--- laravel/apidoc/api_project.js
+++ laravel/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-07-07T12:27:06.933Z",
+    "time": "2016-07-12T07:53:12.147Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
