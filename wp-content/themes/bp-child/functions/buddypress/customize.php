6640fe4b1b02f0b9e49bc92fef27146613212dd2
Ivan Solowjew <ivansolowjew@gmail.com>
2016-07-18 22:06:26 +0300

    #121 Dashboard: "Reports" item is displayed on dashboard

--- wp-content/themes/bp-child/functions/buddypress/customize.php
+++ wp-content/themes/bp-child/functions/buddypress/customize.php
@@ -583,7 +583,6 @@ function getDashboardPages($type = 'page')
                     $item2[] = array('title' => 'Articles', 'url' => $community_url . 'wiki');
                     $item2[] = array('title' => 'Forum', 'url' => $community_url.'forum');
                     $item2[] = array('title' => 'Downloads', 'url' => $community_url . 'downloads');
-                    $item2[] = array('title' => 'Reports', 'url' => $community_url . 'reports');
                     $item2[] = array('title' => 'Surveys', 'url' => $community_url . 'surveys');
                     if (doesUserCommunityAdmin(get_current_user_id(), $community->id)) {
                         $item2[] = array('title' => 'Test Data Backups', 'url' => $community_url . 'backups');
