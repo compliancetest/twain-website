d94db747bd319eeb149a8c6affa91a91471794b4
Ivan Solowjew <ivansolowjew@gmail.com>
2016-08-08 19:07:38 +0300

    #165 Organisation: Page is sensitive to js/css injections - single test suite page fix

--- wp-content/themes/bp-child/single-test-suite.php
+++ wp-content/themes/bp-child/single-test-suite.php
@@ -104,9 +104,10 @@ Template Name Posts: Test Suite
                     <li class="active"><a href="javascript: void(0)" rel="tabs_sv1">Test Suite Roles</a></li>
                     <li><a href="javascript: void(0)" rel="tabs_sv2">Conformance Levels</a><li>
                     <li><a href="javascript: void(0)" rel="tabs_sv3">Profile Types</a></li>
-                    <li><a href="javascript: void(0)" rel="tabs_sv6">Message Templates</a></li>
                     <li><a href="javascript: void(0)" rel="tabs_sv4">Specification Documents</a></li>
-                    <li><a href="javascript: void(0)" rel="tabs_sv5">Related Compliance Suites</a></li>
+                    <?php if(!empty($suite->relatedSuites[0]['id'])):?>
+                        <li><a href="javascript: void(0)" rel="tabs_sv5">Related Compliance Suites</a></li>
+                    <?php endif;?>
 				</ul>
 				
 				<div class="clear"></div>
