c786aefdd22e9e1bd9b882ff409fa04984c34c52
Ilya <ilhanhtml@gmail.com>
2016-10-10 22:18:27 +0300

    #661 - Community Members: Fixed members list when user has long name

--- laravel/resources/views/pages/communities/partials/show/admin-members.blade.php
+++ laravel/resources/views/pages/communities/partials/show/admin-members.blade.php
@@ -56,7 +56,7 @@
             @foreach($community->getAdmins() as $admin)
                 <?php $user = \App\User::find($admin->user_id);?>
                 @if($user)
-                    <li class="col-sm-3">
+                    <li>
                         <label>
                             <input type="checkbox" value="{{ $admin->user_id }}" name="id[]" @if($admin->user_id == Auth::user()->ID) disabled="disabled" @endif>
                             <img width="28" height="28" alt="" class="avatar" src="{{ $user->getAvatar() }}">
@@ -68,6 +68,7 @@
                                 <button type="button" class="btn btn-success btn-sm demoteToMember" data-user-id="{{ $admin->user_id }}">Demote to Member</button>
                             @endif
                         </span>
+                        {{$index}}
                     </li>
                 @endif
             @endforeach
@@ -81,7 +82,7 @@
         @foreach($community->getModerators() as $mod)
             <?php $user = \App\User::find($mod->user_id);?>
             @if($user)
-                <li class="col-sm-3">
+                <li>
                     <label>
                         @if($isAdmin)
                             <input type="checkbox" value="{{ $mod->user_id }}" name="id[]" @if($mod->user_id == Auth::user()->ID) disabled="disabled" @endif>
@@ -106,7 +107,7 @@
         @foreach($community->getMembers() as $member)
             <?php $user = \App\User::find($member->user_id);?>
             @if($user)
-                <li class="col-sm-3">
+                <li>
                     <label>
                         <input type="checkbox" value="{{ $user->ID }}" name="id[]">
                         <img width="28" height="28" alt="" class="avatar" src="{{ $user->getAvatar() }}">
