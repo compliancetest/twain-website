7dd02d175b0a0cb78f76aad71216969c70cc1e01
Ilya <ilhanhtml@gmail.com>
2016-10-13 18:36:31 +0300

    #351 - Explain Test Result. Chat design

--- laravel/resources/views/pages/transactions/popups/explanation-logs.blade.php
+++ laravel/resources/views/pages/transactions/popups/explanation-logs.blade.php
@@ -3,32 +3,37 @@
     Messages
 </div>
 <div class="modal-body">
-    <div class="row">
-        <div class="alert alert-success text-center" role="alert">
-              <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-              Please feel free to ask a question about the test result, we will be happy to help you.
-        </div>
-        @if(!$logs->isEmpty())
-            @foreach($logs as $log)
-                <div class="col-md-12 @if($log->is_support) text-right @endif">
-                    <a href="/members/{{ \App\User::find($log->user_id)->user_nicename }}" target="_blank">{{ cp_get_user_fullname($log->user_id) }}</a><br>
-                    {{ dateDiffForHumans($log->created_at, 'Y-m-d H:i') }}<br>
+    <div class="alert alert-success text-center" role="alert">
+          <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+          Please feel free to ask a question about the test result, we will be happy to help you.
+    </div>
+
+    @if(!$logs->isEmpty())
+        <ul class="chat-list">
+        @foreach($logs as $log)
+            <li class="{{ ($log->is_support) ? 'support-answer' : 'customer-question' }}">
+                <div class="message-author"><a href="/members/{{ \App\User::find($log->user_id)->user_nicename }}" target="_blank">{{ cp_get_user_fullname($log->user_id) }}</a></div>
+                <div class="message-body">
                     {{ $log->message }}
                 </div>
-            @endforeach
-        @else
-             <div class="col-md-12 text-center">
-                No messages yet
-            </div>
-        @endif
+                <div class="message-date">{{ dateDiffForHumans($log->created_at, 'Y-m-d H:i') }}</div>
+            </li>
+        @endforeach
+        </ul>
+    @else
+         <div class="text-center">
+            No messages yet
+        </div>
+    @endif
 
-        <form>
-            <div class="form-group">
-                <input type="hidden" id="transactionId" value="{{ $transactionId }}">
-                <textarea class="form-control" id="explanationMessage" name="explanationMessage"></textarea>
-            </div>
-        </form>
-    </div>
+    <form>
+        <div class="form-group">
+            <input type="hidden" id="transactionId" value="{{ $transactionId }}">
+            <textarea class="form-control" id="explanationMessage" name="explanationMessage"></textarea>
+        </div>
+    </form>
+
+
 </div>
 <div class="modal-footer">
     @if(!($isSupport && $logs->isEmpty()))
