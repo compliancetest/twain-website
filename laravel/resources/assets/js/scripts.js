830a7844d288980c0f1cd08114ec537d0e3ca90f
Ivan Solowjew <ivansolowjew@gmail.com>
2016-10-16 17:07:16 +0300

    #569 Invited Users: "No data yet" notification does not disappear after adding invited users

--- laravel/resources/assets/js/scripts.js
+++ laravel/resources/assets/js/scripts.js
@@ -614,6 +614,7 @@ var Page = {
                 },
                 success: function(rsp, status, jqXHR){
                     if(form.attr('id') == 'invite-user-form'){
+                        $('#settings-members .invitations_table tr:contains("No invited users yet")').remove();
                         $('#settings-members .invitations_table').append('<tr><td>' + rsp.data.invitation_email+ '</td>' +
                                             '<td class="text-center">' +rsp.data.first_name+ ' ' +rsp.data.last_name+'</td>' +
                                             '<td class="text-center">' + rsp.data.created_at +'</td>' +
