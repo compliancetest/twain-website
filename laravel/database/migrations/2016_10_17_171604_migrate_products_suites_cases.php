a01a0359b56e9619e3d5f19d1bd4b63361ca8476
Ivan Solowjew <ivansolowjew@gmail.com>
2016-10-19 15:36:19 +0300

    Added new test suite / test case / product routes

--- laravel/database/migrations/2016_10_17_171604_migrate_products_suites_cases.php
+++ laravel/database/migrations/2016_10_17_171604_migrate_products_suites_cases.php
@@ -24,18 +24,303 @@ class MigrateProductsSuitesCases extends Migration
             $table->string('manufacturer');
             $table->string('protocol_version');
             $table->string('model');
+            $table->text('access_url');
             $table->integer('organisation_id');
-            $table->text('capabilities');
-            $table->text('suites');
+            $table->integer('user_id');
+            $table->integer('wp_id');//..
             $table->timestamps();
         });
+
+         Schema::create('products_features', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('product_id');
+            $table->uuid('test_suites_feature_id');
+            $table->timestamps();
+        });
+
+        Schema::table('products_features', function($table) {
+            $table->foreign('test_suites_feature_id')->references('id')->on('test_suites_features')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+
+        Schema::create('products_capabilities', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('product_id');
+            $table->string('capability');
+            $table->timestamps();
+        });
+
+        Schema::table('products_capabilities', function($table) {
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+
 
         Schema::create('test_suites', function (Blueprint $table) {
             $table->primary('id');
             $table->uuid('id');
+            $table->uuid('community_id');
+            $table->string('slug');
+            $table->string('name');//TWAIN v2.3 Compliance - Data Sources
+            $table->integer('version_major');//1
+            $table->integer('version_minor');//2
+            $table->integer('version_patch');//0
+            $table->string('description');//...
+            $table->string('full_title');//TWAIN v2.3 Compliance - Data Sources v1.2
+            $table->string('short_name');//TWAINDS
+            $table->string('issuer');//twain.org
+            $table->string('revision_description');//First Draft
+            $table->string('status');//Active
+            $table->string('product_type');//DataSource
+            $table->text('excerpt');//DataSource
+            $table->integer('wp_id');//..
+            $table->timestamp('published_at');//2016-01-29
             $table->timestamps();
         });
 
+         Schema::table('test_suites', function($table) {
+            $table->foreign('community_id')->references('id')->on('communities');
+        });
+
+         Schema::create('test_suites_types', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_types', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+         Schema::create('test_suites_conformance_levels', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('code');//A
+            $table->text('description');//Conformance level A represents full compliance with all mandatory components of the TWAIN specification.
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_conformance_levels', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+        Schema::create('test_suites_features', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('name');//Feature1
+            $table->text('description');//Feature1 desc
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_features', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+        Schema::create('test_suites_scenarios', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('code');//SC
+            $table->text('description');//Standard Capability. TWAIN Standard capabilities (ID's with a value less than 0x8000). Ignore Vendor Custom capabilities (ID’s with a value of 0x8000 or greater).
+            $table->integer('sequence');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_scenarios', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+         Schema::create('test_suites_profile_types', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->integer('profile_type_id');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_profile_types', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+        Schema::create('test_suites_related_suites', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->uuid('related_test_suite_id');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_related_suites', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+            $table->foreign('related_test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+         Schema::create('test_suites_roles', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_roles', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+        Schema::create('test_suites_specification_documents', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('name');
+            $table->text('description');
+            $table->text('link');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_specification_documents', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+         Schema::create('test_suites_protocols_versions', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suite_id');
+            $table->string('version');
+            $table->timestamps();
+        });
+
+        Schema::table('test_suites_protocols_versions', function($table) {
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
+
+        Schema::create('test_cases', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->string('slug');
+            $table->string('name');//CN-10
+            $table->integer('version_major');//1
+            $table->integer('version_minor');//2
+            $table->integer('version_patch');//0
+            $table->string('description');//...
+            $table->string('full_title');//CN-10 v1.2
+            $table->string('tester_role');//Application
+            $table->string('harness_role');//DataSource
+            $table->string('initiator');//Tester
+            $table->integer('test_execution_profile_id');//12
+            $table->integer('configuration_profile_id');//12
+            $table->string('outcome_type');//Positive
+            $table->boolean('is_optional');//true
+            $table->string('test_pattern');//..
+            $table->integer('wp_id');//..
+            $table->timestamp('published_at');//2016-01-29
+            $table->timestamps();
+        });
+
+         Schema::create('test_cases_conformance_levels', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->uuid('test_suites_conformance_level_id');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_conformance_levels', function($table) {
+            $table->foreign('test_suites_conformance_level_id')->references('id')->on('test_suites_conformance_levels')->onDelete('cascade');
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+        });
+
+        Schema::create('test_cases_scenarios', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->uuid('test_suites_scenario_id');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_scenarios', function($table) {
+            $table->foreign('test_suites_scenario_id')->references('id')->on('test_suites_scenarios')->onDelete('cascade');
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+        });
+
+        Schema::create('test_cases_roles', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_suites_role_id');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_roles', function($table) {
+            $table->foreign('test_suites_role_id')->references('id')->on('test_suites_roles')->onDelete('cascade');
+        });
+
+        Schema::create('test_cases_samples', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->uuid('image');
+            $table->text('description');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_samples', function($table) {
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+        });
+
+        Schema::create('test_cases_features', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->uuid('test_suites_feature_id');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_features', function($table) {
+            $table->foreign('test_suites_feature_id')->references('id')->on('test_suites_features')->onDelete('cascade');
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+        });
+
+        Schema::create('test_cases_test_steps', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->text('action');
+            $table->text('expected_result');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_test_steps', function($table) {
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+        });
+
+         Schema::create('test_cases_capabilities', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->string('capability');
+            $table->timestamps();
+        });
+
+        Schema::table('test_cases_capabilities', function($table) {
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+        });
+
+        Schema::create('test_suite_test_case', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->uuid('test_case_id');
+            $table->uuid('test_suite_id');
+        });
+
+        Schema::table('test_suite_test_case', function($table) {
+            $table->foreign('test_case_id')->references('id')->on('test_cases')->onDelete('cascade');
+            $table->foreign('test_suite_id')->references('id')->on('test_suites')->onDelete('cascade');
+        });
     }
 
     /**
@@ -45,6 +330,30 @@ class MigrateProductsSuitesCases extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('products_features');
+        Schema::drop('products_capabilities');
+        Schema::drop('products');
+
+        Schema::drop('test_suites_types');
+        Schema::drop('test_suites_conformance_levels');
+        Schema::drop('test_suites_features');
+        Schema::drop('test_suites_scenarios');
+        Schema::drop('test_suites_profile_types');
+        Schema::drop('test_suites_related_suites');
+        Schema::drop('test_suites_roles');
+        Schema::drop('test_suites_specification_documents');
+        Schema::drop('test_suites_protocols_versions');
+
+        Schema::drop('test_cases_conformance_levels');
+        Schema::drop('test_cases_scenarios');
+        Schema::drop('test_cases_roles');
+        Schema::drop('test_cases_samples');
+        Schema::drop('test_cases_features');
+        Schema::drop('test_cases_test_steps');
+        Schema::drop('test_cases_capabilities');
+
+        Schema::drop('test_suite_test_case');
+        Schema::drop('test_cases');
+        Schema::drop('test_suites');
     }
 }
