59bf64fb2f757a2aef1477737212798443671e50
Ilya <ilhanhtml@gmail.com>
2016-11-05 19:39:29 +0200

    #767 - Coverage: Fixed Tool-tip for the last test case in a row

--- laravel/resources/views/pages/my/coverage/test_plans_list.blade.php
+++ laravel/resources/views/pages/my/coverage/test_plans_list.blade.php
@@ -36,14 +36,13 @@
                                     <td class="col-sm-1 text-nowrap text-center">
                                         @if(!\App\Transaction::where(['product_id' => $userPlan['product']->ID, 'test_suite_id' => $userSuite['testSuite']->ID])->get()->isEmpty())
                                             <a href="/my-transaction-log/?test_suite_id={{ $userSuite['testSuite']->ID }}&product_id={{ $userPlan['product']->ID }}" target="_blank"
-                                               class="btn btn-primary btn-icon btn-view" data-tooltip="tooltip" title="View log"></a>
+                                               class="btn btn-primary btn-icon btn-view" data-tooltip="tooltip" data-container="body" title="View log"></a>
                                         @endif
 
                                         <a href="/testplan/{{ $userPlan['testPlan']->id }}/edit" data-toggle="modal" data-remote="true" data-ajax-modal data-target="#editPlanModal"
-                                           class="btn btn-primary btn-icon btn-edit" data-tooltip="tooltip" title="Edit plan"></a>
+                                           class="btn btn-primary btn-icon btn-edit" data-tooltip="tooltip"  data-container="body" title="Edit plan"></a>
                                         <a href="#removePlanModal-{{ $userPlan['testPlan']->id }}" data-toggle="modal" class="btn btn-danger btn-icon btn-delete"
-                                           data-tooltip="tooltip"
-                                           title="Delete plan"></a>
+                                           data-tooltip="tooltip" data-container="body" title="Delete plan"></a>
                                         @if($userPlan['testPlan']->canBeClaimed() && !$userPlan['testPlan']->is_claimed)
                                             <a href="/testplan/{{ $userPlan['testPlan']->id }}/claim" class="btn btn-success btn-icon btn-confirm" data-tooltip="tooltip"
                                                title="Claim"></a>
