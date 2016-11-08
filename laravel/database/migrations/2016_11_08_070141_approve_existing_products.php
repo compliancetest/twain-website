7ac822edef304de89b7506b506aa8e1e1335fa5f
Ivan Solowjew <ivansolowjew@gmail.com>
2016-11-08 09:25:17 +0200

    products bulk approve script

---
+++ laravel/database/migrations/2016_11_08_070141_approve_existing_products.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ApproveExistingProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \App\CommunityOrganisationsApprovedProducts::truncate();
+        $organisations = \App\Organisation::all();
+        foreach (\App\Community::all() as $community) {
+            foreach ($organisations as $organisation) {
+                $organisationProducts = $organisation->getProducts();
+                foreach ($organisationProducts as $product) {
+                    \App\CommunityOrganisationsApprovedProducts::create([
+                        'organisation_id' => $organisation->id,
+                        'community_id' => $community->id,
+                        'product_id' => $product->ID,
+                        'approved_by' => 1
+                    ]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
