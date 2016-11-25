af7fc002719a907ecac40b57fd702aad6ad6ef16
Ivan Solowjew <ivansolowjew@gmail.com>
2016-11-25 18:54:20 +0200

    #819 API Logs: migrate data to s3

--- laravel/database/migrations/2016_11_25_135232_move_apilogs_data_to_s3.php
+++ laravel/database/migrations/2016_11_25_135232_move_apilogs_data_to_s3.php
@@ -12,10 +12,27 @@ class MoveApilogsDataToS3 extends Migration
      */
     public function up()
     {
-        foreach (\App\ApiLog::all() as $apilog){
-            \Illuminate\Support\Facades\Storage::put('apilogs/' . $apilog->id . '/request.json', $apilog->request);
-            \Illuminate\Support\Facades\Storage::put('apilogs/' . $apilog->id . '/response.json', $apilog->response);
+        if(!file_exists(storage_path() . "/apilogs")) {
+            mkdir(storage_path() . "/apilogs", 0777);
         }
+        if(!file_exists(storage_path() . "/apilogs/apilogs")) {
+            mkdir(storage_path() . "/apilogs/apilogs", 0777);
+        }
+        $path = storage_path() . "/apilogs/";
+        $s3 = \Aws\Laravel\AwsFacade::createClient('s3');
+        foreach (\App\ApiLog::all() as $apilog){
+            if(!file_exists(storage_path() . "/apilogs/apilogs/" . $apilog->id)) {
+                mkdir(storage_path() . "/apilogs/apilogs/" . $apilog->id, 0777);
+            }
+            file_put_contents($path . 'apilogs/'. $apilog->id . '/request.json', $apilog->request);
+            file_put_contents($path .  'apilogs/'. $apilog->id . '/response.json', $apilog->response);
+        }
+        try {
+            $status = $s3->uploadDirectory($path, config('env.bucket.website'));
+
+        } catch (Exception $e) {
+        }
+
         Schema::table('api_logs', function ($table) {
             $table->dropColumn(['request', 'response']);
         });
