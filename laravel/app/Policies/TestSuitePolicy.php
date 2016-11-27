afc66644194993d47d9751c88a85bfb1b60d1d78
Ivan Solowjew <ivansolowjew@gmail.com>
2016-11-27 09:35:37 +0200

    #811 Test Suites: It is possible to subscibe to new minor/major version of a test suite which is not approved for a testing

--- laravel/app/Policies/TestSuitePolicy.php
+++ laravel/app/Policies/TestSuitePolicy.php
@@ -51,30 +51,15 @@ class TestSuitePolicy
     }
 
     /**
-     * Check that user can view Test Suite
+     * Check that user can subscribe to Test Suite
      * @param User $user
      * @param LaravelTestSuite $testSuite
      * @return bool
      */
     public function subscribeToTestSuite(User $user, LaravelTestSuite $testSuite)
     {
-        $community = Community::find($testSuite->community_id);
-        if ($community->visibility_status == 'private') {
-            if ($testSuite->status == 'Active' || $community->isAdmin()) {
-                return true;
-            }
-            if (($community->isModerator() || $community->getMember($user->ID)) && in_array($testSuite->status, ['Active', 'Partial', 'Obsolete', 'Deprecated'])) {
-                return true;
-            }
-        }
-        if ($community->visibility_status == 'public') {
-            //only community member can subscribe
-            if (!$community->getMember($user->ID)){
-                return false;
-            }
-            if ($testSuite->status == 'Active' || $community->isAdmin() || in_array($testSuite->status, ['Active', 'Partial', 'Obsolete', 'Deprecated'])) {
-                return true;
-            }
+        if ($testSuite->status == 'Active') {
+            return true;
         }
         return false;
     }
