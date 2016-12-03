caa93108d5c1e431a2edb381ca657091e5eda1b4
Ivan Solowjew <ivansolowjew@gmail.com>
2016-12-03 13:32:01 +0200

    #856 Test Suites: release subscription

--- laravel/resources/views/pages/test-suites/partials/subscriptions-section.blade.php
+++ laravel/resources/views/pages/test-suites/partials/subscriptions-section.blade.php
@@ -6,7 +6,7 @@
         </div>
         @include('pages.test-suites.partials.release-subscription')
     @else
-        @if(Auth::user()->doesUserOrganisationApproved($testSuite) && $community->getActiveMember(Auth::user()->ID))
+        @if(Auth::user()->doesUserOrganisationApproved($testSuite) && $testSuite->community->getActiveMember(Auth::user()->ID))
             @can('subscribeToTestSuite', $testSuite)
                 @include('pages.test-suites.partials.assign-subscription')
             @endcan
